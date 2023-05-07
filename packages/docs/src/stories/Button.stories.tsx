4b641e37f458427b12189289b5cf7c9c0cfe1dba
Felipe Leal <felipewanderson2020@gmail.com>
2023-05-07 00:20:19 -0300

    fix: pages docs tokens

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '@felipeignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
